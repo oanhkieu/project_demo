a9ecd77e4e9e4f186178c0c98496f78733cfe043
Oanh Kieu <hankieuoanh1892@gmail.com>
2016-03-03 14:01:11 +0700

    Them connect

--- connect.php
+++ connect.php
@@ -8,11 +8,11 @@
 	}else{
 		echo 'Kết nối thành công';
 	}
-	// $sql='create database project_demo';
-	// if(($conn ->query($sql))==true){
-	// 	echo 'Tạo thành công';
-	// }else{
-	// 	echo 'Không thành công'.$conn->error;
-	// }
+	$sql='create database project_demo';
+	if(($conn ->query($sql))==true){
+		echo 'Tạo thành công';
+	}else{
+		echo 'Không thành công'.$conn->error;
+	}
 
 ?>
